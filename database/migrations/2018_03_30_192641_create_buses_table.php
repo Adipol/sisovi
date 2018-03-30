a7a32ae6460c4b7f62dbb76b6310e6440f8fa908
George <adipol13@gmail.com>
2018-03-30 16:30:19 -0400

    creacionTablas

---
+++ database/migrations/2018_03_30_192641_create_buses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buses', function (Blueprint $table) {
+			$table->increments('id');
+			
+			$table->string('code')->unique();
+			$table->string('license_plate')->unique();
+
+			$table->integer('patios_id')->unsigned();
+			$table->foreign('patios_id')->references('id')->on('patios');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buses');
+    }
+}
