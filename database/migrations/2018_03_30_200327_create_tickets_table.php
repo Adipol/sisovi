2df503a27a5321d2c2ef68842bb83cd6f72132c5
George <adipol13@gmail.com>
2018-03-30 17:00:31 -0400

    tablaTikets

--- database/migrations/2018_03_30_200327_create_tickets_table.php
+++ database/migrations/2018_03_30_200327_create_tickets_table.php
@@ -16,11 +16,16 @@ class CreateTicketsTable extends Migration
         Schema::create('tickets', function (Blueprint $table) {
 			$table->increments('id');
 
+			$table->integer('applicant_id');//solicitante id
+			$table->integer('operational_id');//operacional id
+			$table->time('start_time'); //hora de inico de incidente
+			$table->time('ending_time'); //hora de finalizacion de incidente
+			
 			$table->date('incident_date');//fecha de incidente
 			$table->text('applicant_obs');//solicitante
 			$table->text('operational_obs');//operacional
 			
-			
+
 
             $table->timestamps();
         });
