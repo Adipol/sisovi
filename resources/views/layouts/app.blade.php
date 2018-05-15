4080dc95ba56a257f1175ab6359d77544c32d25d
jorge <adipol13@hotmail.com>
2018-05-14 20:49:30 -0400

    Se elimino una fuente desconocida

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'SISOVI') }}</title>
     <!-- Fonts -->
-  	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css"> 
     <!-- Styles -->
 	<link rel="stylesheet" href="{{ asset('css/boots.css')}}">
