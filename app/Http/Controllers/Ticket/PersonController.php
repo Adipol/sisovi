4af9af2843c4049d96f41b53a789e44f2d955334
george <adipol13@gmail.com>
2018-05-22 19:25:42 -0400

    se cambio el header del ajax

--- app/Http/Controllers/Ticket/PersonController.php
+++ app/Http/Controllers/Ticket/PersonController.php
@@ -5,28 +5,30 @@ namespace App\Http\Controllers\Ticket;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PersonStoreRequest;
-use Validator;
-use Illuminate\Support\Facades\Input;
 //use Illuminate\Http\Response;
-
-use Response;
 use App\Person;
 
 class PersonController extends Controller
 {
-    public function store(Request $request){
-		
-		$validator=Validator::make($request->all(),[
-			'name'          => 'required',
-			'firstName'     => 'required',
-			'lastName'      => 'required',
-			'identity_card' => 'required',
-			'issued'        => 'required'
-		]);
-		
-		$input=$request->all();
+	public function store(Request $request)
+	{	
+		$messages = [
+			'name.required' => 'Necesita ingersar el nombre!',
+		];
 
-       if($validator->passes()){
+		$validator=\Validator::make($request->all(),[
+			'name'=>'required',
+			'firstName'=>'required',
+			'LastName'=>'min:3',
+			'identity_card'=>'required',
+			'issued'=>'required',
+		],$messages);
+		
+        if ($validator->fails())
+        {
+            return response()->json(['errors'=>$validator->errors()->all()]);
+        }
+
 		$person                = new Person();
 		$person->firstName     = $request->get('firstName');
 		$person->lastName      = $request->get('lastName');
@@ -36,13 +38,8 @@ class PersonController extends Controller
 		$ucm                   = auth()->user();
 		$person->ucm           = $ucm->id;
 		$person->save();
-		
-		return Response::json(['success'=>'1']);
 
-	   }
-	   return Response::json(['erros'=>$validator->errors()]);
-		
-
+		return response()->json(['success'=>'Data is successfully added']);
 	
 	}
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>{{ config('app.name', 'SISOVI') }}</title>
     <!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css"> 
@@ -25,7 +26,7 @@
 	
 	<!-- Scripts -->
 	<script src="{{ asset('js/jquery.min.js') }}"></script>
-	<script src="{{ asset('js/app.js') }}"></script> 
+	<script src="{{ asset('js/app.js') }}"></script>
 	@stack('scripts')
 </body>
 </html>
